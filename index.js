a0e4977a7d27c1687d2618352bed2e827a77d043
Harvey Rendell <harvey.rendell@pushpay.com>
2018-09-17 09:25:00 +1200

    Initial setup for time command handling and data layout. Incomplete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client({autoReconnect: true});
+const bot = new Discord.Client({ autoReconnect: true });
 const credentials = require('./credentials.json');
+const TZdata = require('./data.json');
+
 
 bot.login(process.env.TOKEN || credentials.discordToken);
 
@@ -9,7 +11,42 @@ bot.on('ready', function (event) {
 });
 
 bot.on('message', function (message) {
-  if (!message.author.bot) {
-    message.channel.send('Hey! o/');
+  command = validate(message)
+  if (!command) return;
+  command.shift(); // Strip mention from command
+  console.log("command = [" + command + "]")
+
+  arg = command.shift()
+  switch (arg) {
+    case "time":
+      response = getTZInfo(command)
+      break;
+    default:
+      response = "";
   }
+
+  if (response) message.channel.send(response);
 });
+
+function validate(message) {
+  if (message.author.bot) return; // Ignore self
+
+  let originalCommand = message.content.match(/[^\s"]+|"(?:[^"\\]|\\")*"/g) || []; // Help servers are useless
+  let command = originalCommand.map(str => {
+    // Unescape quotes in the string target if it's a quoted string and trim the start and end quotes
+    if (str.charAt(0) === '"' && str.charAt(str.length - 1) === '"' && str.length > 1) {
+      str = str.substring(1, str.length - 1).trim().replace(/\\"/g, '"');
+    }
+    return str;
+  });
+  if (command[0] !== bot.user.toString()) return;
+
+  return command
+}
+
+function getTZInfo(args) {
+  console.log("Timezone info called with [" + args + "]")
+  TZdata.zones.forEach(function (zone) {
+    console.log(zone)
+  });
+}
